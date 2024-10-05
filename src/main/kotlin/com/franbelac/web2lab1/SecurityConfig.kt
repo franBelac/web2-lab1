665ae5c07ac5e30152fb1f711c194ef29641d7e0
Fran Belac <fran.belac@porsche.digital>
2024-10-05 23:21:31 +0200

    Tweak security config

--- src/main/kotlin/com/franbelac/web2lab1/SecurityConfig.kt
+++ src/main/kotlin/com/franbelac/web2lab1/SecurityConfig.kt
@@ -2,7 +2,6 @@ package com.franbelac.web2lab1
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
@@ -12,30 +11,18 @@ import org.springframework.security.config.annotation.web.invoke
 @EnableWebSecurity
 class SecurityConfig {
     @Bean
-    @Order(1)
-    fun resourceServerFilterChain(http: HttpSecurity): SecurityFilterChain {
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http {
             authorizeHttpRequests {
                 authorize("/api/tickets/total",permitAll)
                 authorize("/api/tickets",authenticated)
+                authorize("/api/tickets/**",hasRole("USER"))
             }
             oauth2ResourceServer {
                 jwt {  }
             }
-        }
-        return http.build()
-    }
-
-    @Bean
-    @Order(2)
-    fun loginFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http {
-            authorizeHttpRequests {
-                authorize("/api/tickets/**",authenticated)
-            }
             oauth2Login {  }
         }
-
         return http.build()
     }
 
