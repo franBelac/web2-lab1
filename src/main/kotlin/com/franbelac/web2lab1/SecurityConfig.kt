678fc6386d67e8d47cfb42b4b061b4c8992bb157
Fran Belac <fran.belac@porsche.digital>
2024-10-12 19:41:27 +0200

    change config

--- src/main/kotlin/com/franbelac/web2lab1/SecurityConfig.kt
+++ src/main/kotlin/com/franbelac/web2lab1/SecurityConfig.kt
@@ -3,34 +3,38 @@ package com.franbelac.web2lab1
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
 
     @Bean
-    @Order(1)
+    @Order(2)
     fun loginFilter(http: HttpSecurity): SecurityFilterChain {
         http {
             authorizeHttpRequests {
-                authorize("/api/tickets/overview/**",authenticated)
+                authorize(method = HttpMethod.GET, pattern = "/api/tickets/overview/**",authenticated)
+                authorize(method = HttpMethod.GET, pattern = "/api/tickets/total",permitAll)
             }
             oauth2Login {  }
         }
+
         return http.build()
     }
 
     @Bean
-    @Order(2)
+    @Order(1)
     fun resourceServerFilter(http: HttpSecurity) : SecurityFilterChain {
         http {
+            securityMatcher(RequestHeaderRequestMatcher("Authorization"))
             authorizeHttpRequests {
-                authorize("/api/tickets",authenticated)
-                authorize("/api/tickets/total",permitAll)
+                authorize(method = HttpMethod.POST, pattern = "/api/tickets",authenticated)
             }
 
             oauth2ResourceServer {
